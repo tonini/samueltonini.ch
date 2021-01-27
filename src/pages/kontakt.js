f6b19c137bd413c37d4b6464b7a671f4d0d69967
Samuel Tonini <tonini.samuel@gmail.com>
2021-01-27 15:43:14 +0100

    add praxisgemeinschaft therapeuten

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -38,14 +38,28 @@ const KontaktPage = () => (
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2723.124629243025!2d7.432922615676176!3d46.95924127914701!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478e3990c6aeee1f%3A0xd2ab8f4f7f07db71!2sHochfeldstrasse%20117%2C%203012%20Bern!5e0!3m2!1sde!2sch!4v1611432719823!5m2!1sde!2sch"
           width="920"
-          height="300"
+          height="250"
           frameborder="0"
           style={{ border: 0 }}
           allowfullscreen=""
           aria-hidden="false"
-
           title="Samuel Tonini | Praxis für Chinesische Medizin Akupunktur & Tuina / Anmo"
         ></iframe>
+      </div>
+    </div>
+
+    <div class="site-half">
+      <div class="col">
+        <br />
+        <h2 class="left">In Praxisgemeinschaft mit:</h2>
+        <p>
+          <b>Rahel Fankhauser</b>, Dipl. TCM Therapeutin Akupunktur & Tuina
+          <br />
+          <a href="https://momen.ch/" target="_blank" rel="noreferrer">www.momen.ch</a>
+        </p>
+        <p>
+          <b>Rafael Fernandez</b>, Dipl. TCM Therapeut Akupunktur & Tuina
+        </p>
       </div>
     </div>
   </Layout>
